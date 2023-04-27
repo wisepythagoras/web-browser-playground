bf0f3df8aa33cd43a18438edfb41fd588395cb22
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2023-04-26 22:02:32 -0400

    Revive js clipboard example

--- index.js
+++ index.js
@@ -16,15 +16,15 @@ console.error(JSON.stringify(navigator.languages), host);
 console.warn(function() {});
 console.log(console.warn.toString());
 
-// clipboard.writeText().catch((err) => console.error('Expected fail:', err));
-// clipboard.writeText('Lorem ipsum dolor sit amet, consectetur adipiscing elit')
-//     .then(() => {
-//         console.log('Copied');
-//         clipboard.readText().then((data) => {
-//             console.log(`Clipboard data: ${data}`);
-//         });
-//     })
-//     .catch((e) => console.error(e));
+clipboard.writeText().catch((err) => console.error('Expected fail:', err));
+clipboard.writeText('Lorem ipsum dolor sit amet, consectetur adipiscing elit')
+    .then(() => {
+        console.log('Copied');
+        clipboard.readText().then((data) => {
+            console.log(`Clipboard data: ${data}`);
+        });
+    })
+    .catch((e) => console.error(e));
 
 console.log('Here');
 myfn2(234).then((r) => {
